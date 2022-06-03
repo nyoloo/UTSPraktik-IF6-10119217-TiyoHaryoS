d4d539ce2069047b9724935215f07b9afb3dfed4
nyoloo <87368516+nyoloo@users.noreply.github.com>
2022-06-03 18:41:22 +0700

    UTS

---
+++ app/src/main/java/com/nyolo/utspraktik_if6_10119217_tiyoharyos/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nyolo.utspraktik_if6_10119217_tiyoharyos;
+// Tiyo Haryo Subaktiono
+// IF 6
+// 10119217
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
